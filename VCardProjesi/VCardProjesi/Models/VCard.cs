0644a1d17643969cfefc67deba1f84cbce3cd8e2
unknown <shabnam.sadikhzada@gmail.com>
2024-07-04 21:01:34 +0400

    not ready project added

---
+++ VCardProjesi/VCardProjesi/Models/VCard.cs
@@ -0,0 +1,35 @@
+﻿namespace VCardProjesi.Models;
+
+public class VCard
+{
+    public Name Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public Location Location { get; set; }
+
+    public string VCardDonusturme()
+    {
+        string res = 
+            $"BEGIN  :  VCARD\r\n" +
+            $"VERSION:  3.0\r\n" +
+            $"N      :  {Name.First};{Name.Last}\r\n" +
+            $"EMAIL  :  {Email}\r\n" +
+            $"TEL    :  {Phone}\r\n" +
+            $"ADR    :  {Location.City};{Location.Country}\r\n" +
+            $"END    :  VCARD\r\n";
+
+        return res;
+    }
+}
+
+public class Name
+{
+    public string First { get; set; }
+    public string Last { get; set; }
+}
+
+public class Location
+{
+    public string City { get; set; }
+    public string Country { get; set; }
+}
---
+++ VCardProjesi/VCardProjesi/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using VCardProjesi.Models;
+
+namespace VCardProjesi;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("lutfen kac adet vcard olusturmak istediginizi giriniz: ");
+        int count = Convert.ToInt32(Console.ReadLine());
+
+        string url = "https://randomuser.me/api/?results=1";
+        using HttpClient client = new HttpClient();
+
+        string filePath = "../../../VCards/";
+
+
+        for (int i = 0;i < count; i++)
+        {
+            using HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            //var vCards = JsonConvert.DeserializeObject<VCard[]>(responseBody);  //bunu ben yazdım ama yanlış, nedenini bilmiyorum
+            //aşağıdakı 3 satırı gpt yazdı, anladım mı ? hayır
+            var jsonObject = JsonConvert.DeserializeObject<JObject>(responseBody);
+            var results = jsonObject["results"].ToString();
+            var vCards = JsonConvert.DeserializeObject<VCard[]>(results);
+            
+            foreach (var vcard in vCards)
+            {
+                string vcardStr = vcard.VCardDonusturme();
+                Console.WriteLine(vcardStr);
+                filePath = Path.Combine(filePath, $"{vcard.Name.First}_{vcard.Name.Last}.vcf");
+                File.Create(filePath);
+                //File.WriteAllText(filePath, vcardStr);
+
+                //using (StreamWriter writer = new StreamWriter(filePath))
+                //{
+                //    writer.Write(vcardStr);
+                //}
+            }
+        }
+
+
+        Console.ReadKey();
+        
+    }
+}
